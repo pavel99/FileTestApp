6ca44084d732d028cf4662b8ab4986470b919948
pavel99 <mailforgithub@gmail>
2015-05-13 14:10:50 +0600

    Second Commit

---
+++ FileTestApp/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace FileTestApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputNameTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.showListBox = new System.Windows.Forms.ListBox();
+            this.ShowButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // inputNameTextBox
+            // 
+            this.inputNameTextBox.Location = new System.Drawing.Point(72, 13);
+            this.inputNameTextBox.Name = "inputNameTextBox";
+            this.inputNameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.inputNameTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(197, 12);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(75, 23);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // showListBox
+            // 
+            this.showListBox.FormattingEnabled = true;
+            this.showListBox.Location = new System.Drawing.Point(81, 102);
+            this.showListBox.Name = "showListBox";
+            this.showListBox.Size = new System.Drawing.Size(120, 95);
+            this.showListBox.TabIndex = 3;
+            // 
+            // ShowButton
+            // 
+            this.ShowButton.Location = new System.Drawing.Point(97, 215);
+            this.ShowButton.Name = "ShowButton";
+            this.ShowButton.Size = new System.Drawing.Size(75, 23);
+            this.ShowButton.TabIndex = 4;
+            this.ShowButton.Text = "Show";
+            this.ShowButton.UseVisualStyleBackColor = true;
+            this.ShowButton.Click += new System.EventHandler(this.ShowButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.ShowButton);
+            this.Controls.Add(this.showListBox);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputNameTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputNameTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.ListBox showListBox;
+        private System.Windows.Forms.Button ShowButton;
+    }
+}
+
---
+++ FileTestApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileTestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string path = @"E:\Pavel\29.05.15\FileTestApp\FileTestApp\info.txt";
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            //string path = @"E:\Pavel\29.05.15\FileTestApp\FileTestApp\info.txt";
+            FileStream aStream =new FileStream(path,FileMode.Append);
+            StreamWriter aStreamWriter=new StreamWriter(aStream);
+            aStreamWriter.WriteLine(inputNameTextBox.Text);
+            aStreamWriter.Close();
+            aStream.Close();
+            inputNameTextBox.Clear();
+        }
+
+        private void ShowButton_Click(object sender, EventArgs e)
+        {
+            showListBox.Items.Clear();
+            FileStream afilFileStream = new FileStream(path, FileMode.Open);
+            StreamReader aReader=new StreamReader(afilFileStream);
+            while(!aReader.EndOfStream)
+            {
+                string name = aReader.ReadLine();
+                showListBox.Items.Add(name);
+            }
+            aReader.Close();
+            afilFileStream.Close();
+        }
+    }
+}
